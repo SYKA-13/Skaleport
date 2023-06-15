3738f8f72ab753a20ee0f27554a9fcab6c44b0b2
SYKA-13 <syedkalam13@gmail.com>
2023-06-15 20:55:25 +0300

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import Theme from '../styles/theme';
-
+import {Analytics} from '@vercel/analytics/react';
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
         <Component {...pageProps} />
       </Theme>
+      <Analytics/>
     </>
+   
   );
 }
  
